978dbed30f8508cef5d210be39be954a174c269d
cssxsh <cssxsh@gmail.com>
2023-02-12 20:31:07 +0800

    feat: props

--- src/main/kotlin/xyz/cssxsh/rosu/Props.kt
+++ src/main/kotlin/xyz/cssxsh/rosu/Props.kt
@@ -1,3 +1,12 @@
 package xyz.cssxsh.rosu
 
-internal const val PRETTY_PRINT_KEY: String = "xyz.cssxsh.osu.debug.pretty"
+internal const val PRETTY_PRINT_KEY: String = "xyz.cssxsh.rosu.debug.pretty"
+
+@Suppress("NOTHING_TO_INLINE")
+internal inline fun pretty(): Boolean {
+    val value = System.getProperty(PRETTY_PRINT_KEY)
+        ?: System.getenv("ROSU_DEBUG_PRETTY")
+        ?: "false"
+
+    return value.equals("true", true)
+}
