d6ba5aaed559227c3f78adb28b5c13d9369183c5
cssxsh <cssxsh@gmail.com>
2023-02-13 21:24:42 +0800

    fix: AnyPP Test

--- src/test/kotlin/xyz/cssxsh/rosu/AnyPPTest.kt
+++ src/test/kotlin/xyz/cssxsh/rosu/AnyPPTest.kt
@@ -19,18 +19,18 @@ internal class AnyPPTest {
                 val difficulty = attributes.difficulty
                 assertEquals(expected, difficulty.mode)
                 assertNotEquals(0, difficulty.ptr)
-                assertEquals(2.8693628443424104, difficulty.aim)
-                assertEquals(2.533869745015772, difficulty.speed)
-                assertEquals(2.288770487900865, difficulty.flashlight)
-                assertEquals(0.9803052946037858, difficulty.sliderFactor)
-                assertEquals(210.36373973116545, difficulty.speedNoteCount)
-                assertEquals(9.300000190734863, difficulty.ar)
-                assertEquals(8.800000190734863, difficulty.od)
-                assertEquals(5.0, difficulty.hp)
+                assertEquals(2.8693628443424104, difficulty.aim, 0.000_0001)
+                assertEquals(2.533869745015772, difficulty.speed, 0.000_0001)
+                assertEquals(2.288770487900865, difficulty.flashlight, 0.000_0001)
+                assertEquals(0.9803052946037858, difficulty.sliderFactor, 0.000_0001)
+                assertEquals(210.36373973116545, difficulty.speedNoteCount, 0.000_0001)
+                assertEquals(9.300000190734863, difficulty.ar, 0.000_0001)
+                assertEquals(8.800000190734863, difficulty.od, 0.000_0001)
+                assertEquals(5.0, difficulty.hp, 0.000_0001)
                 assertEquals(307, difficulty.circles)
                 assertEquals(293, difficulty.sliders)
                 assertEquals(1, difficulty.spinners)
-                assertEquals(5.669858729379628, difficulty.stars)
+                assertEquals(5.669858729379628, difficulty.stars, 0.000_0001)
                 assertEquals(909, difficulty.maxCombo)
             }
             GameMode.Taiko -> {
@@ -45,12 +45,12 @@ internal class AnyPPTest {
                 val difficulty = attributes.difficulty
                 assertEquals(expected, difficulty.mode)
                 assertNotEquals(0, difficulty.ptr)
-                assertEquals(1.4528845068865617, difficulty.stamina)
-                assertEquals(0.20130047251681948, difficulty.rhythm)
-                assertEquals(1.0487315549761433, difficulty.colour)
-                assertEquals(1.8881824429738323, difficulty.peak)
-                assertEquals(35.0, difficulty.hitWindow)
-                assertEquals(2.9778030386845606, difficulty.stars)
+                assertEquals(1.4528845068865617, difficulty.stamina, 0.000_0001)
+                assertEquals(0.20130047251681948, difficulty.rhythm, 0.000_0001)
+                assertEquals(1.0487315549761433, difficulty.colour, 0.000_0001)
+                assertEquals(1.8881824429738323, difficulty.peak, 0.000_0001)
+                assertEquals(35.0, difficulty.hitWindow, 0.000_0001)
+                assertEquals(2.9778030386845606, difficulty.stars, 0.000_0001)
                 assertEquals(289, difficulty.maxCombo)
             }
             GameMode.Catch -> {
@@ -65,8 +65,8 @@ internal class AnyPPTest {
                 val difficulty = attributes.difficulty
                 assertEquals(expected, difficulty.mode)
                 assertNotEquals(0, difficulty.ptr)
-                assertEquals(3.2502669316166624, difficulty.stars)
-                assertEquals(8.0, difficulty.ar)
+                assertEquals(3.2502669316166624, difficulty.stars, 0.000_0001)
+                assertEquals(8.0, difficulty.ar, 0.000_0001)
                 assertEquals(728, difficulty.fruits)
                 assertEquals(2, difficulty.droplets)
                 assertEquals(291, difficulty.tinyDroplets)
@@ -83,8 +83,8 @@ internal class AnyPPTest {
                 val difficulty = attributes.difficulty
                 assertEquals(expected, difficulty.mode)
                 assertNotEquals(0, difficulty.ptr)
-                assertEquals(4.824631127426499, difficulty.stars)
-                assertEquals(40.0, difficulty.hitWindow)
+                assertEquals(4.824631127426499, difficulty.stars, 0.000_0001)
+                assertEquals(40.0, difficulty.hitWindow, 0.000_0001)
                 assertEquals(5064, difficulty.maxCombo)
             }
         }
